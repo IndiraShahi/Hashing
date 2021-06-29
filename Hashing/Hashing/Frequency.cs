7c7709ccc60ba012650f6d58729716d0df921693
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-29 21:31:57 +0530

    [Indira] ADDED: Find frequency of words from a paragraph

---
+++ Hashing/Hashing/Frequency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Hashing
+{
+    class Frequency
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void Times(MyMapNode<int, string> hash)
+        {
+            for (int key = 0; key < hash.size; key++)
+            {
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine("Frequency of word " + item.Key + " is " + item.Value);
+            }
+
+        }
+    }
+}
--- Hashing/Hashing/MyMapNode.cs
+++ Hashing/Hashing/MyMapNode.cs
@@ -5,16 +5,18 @@ namespace Hashing
 {
     class MyMapNode<K, V>
     {
-        private readonly int _size;
+        public readonly int size;
         private readonly LinkedList<KeyValue<K, V>>[] items;
+
         public MyMapNode(int size)
         {
-            this._size = size;
+            this.size = size;
             this.items = new LinkedList<KeyValue<K, V>>[size];
         }
+
         public int GetArrayPosition(K key)
         {
-            int position = key.GetHashCode() % _size;
+            int position = key.GetHashCode() % size;
             return Math.Abs(position);
         }
 
@@ -51,30 +53,10 @@ namespace Hashing
             return linkedList;
         }
 
-        public int GetFrequencyOfWords(V value)
+        public struct KeyValue<S, T>
         {
-            int count = 0;
-            if (items == null)
-            {
-                Console.WriteLine("Hash Table is Empty!");
-                return 0;
-            }
-            for (int i = 0; i < items.Length; i++)
-            {
-                LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(i);
-                foreach (KeyValue<K, V> item in linkedList)
-                {
-                    if (item.value.Equals(value))
-                        count++;
-                }
-            }
-            return count;
-        }
-
-        public struct KeyValue<K, V>
-        {
-            public K key { get; set; }
-            public V value { get; set; }
+            public S key { get; set; }
+            public T value { get; set; }
         }
     }
 }
