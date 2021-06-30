b7bf6c8b8cd2b4a60b9b4fe1313f3e803fe63c64
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-30 21:22:34 +0530

    [Indira] ADDED: Create a BST with 56 as root and 30 and 70 as left node and right node.

---
+++ BinarySearchTree/BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BinaryTree<T> where T : IComparable 
+    {                                         
+        Node<T> Root;
+        Node<T> Current;
+        
+        public void InsertData(T data)
+        {
+           
+            if (Root == null)
+            {
+                this.Root = new Node<T>(data);
+                this.Current = Root;
+                return;
+            }
+            
+            if (this.Current.data.CompareTo(data) > 0)
+            {
+                
+                if (this.Current.leftNode == null)
+                {
+                    this.Current.leftNode = new Node<T>(data);
+                    this.Current = Root;
+
+                }
+                else
+                {
+                    this.Current = this.Current.leftNode;
+                    InsertData(data);
+                }
+                Console.WriteLine("{0} Left Node value ", data);
+            }
+            else
+            {
+                ///Checks if right node is null.
+                if (this.Current.rightNode == null)
+                {
+                    this.Current.rightNode = new Node<T>(data);
+                    this.Current = Root;
+                }
+                else
+                {
+                    this.Current = this.Current.rightNode;
+                    InsertData(data);
+                }
+                Console.WriteLine("{0} Right Node value ", data);
+            }
+
+        }
+        
+        public Node<T> GetRoot()
+        {
+            return this.Root;
+        }
+        
+        public void Display(Node<T> node)
+        {
+            if (node != null)
+            {
+                Display(node.leftNode);
+                Console.WriteLine("Numbers in binary tree: " + node.data);
+                Display(node.rightNode);
+            }
+
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to binary search tree algorithm");
+            BinaryTree<int> tree = new BinaryTree<int>();
+            tree.InsertData(56);
+            tree.InsertData(30);
+            tree.InsertData(70);
+            tree.Display(tree.GetRoot());
+        }
+    }
+}
